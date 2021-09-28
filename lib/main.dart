296ac659270f76e6d22f6e40c699da6f5ff027dc
qdnerak <neraknerakneraknerak@gmail.com>
2021-09-28 11:13:20 -0400

    BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hikeplaces/places.dart';
 import 'home.dart';
 
 void main(){
@@ -14,7 +15,7 @@ class MyApp  extends StatelessWidget{
         visualDensity: VisualDensity.adaptivePlatformDensity,
 
       ),
-      home: MyHome(),
+      home: Places(),
     );
 
   }
